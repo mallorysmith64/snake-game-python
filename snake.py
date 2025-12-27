6767c0ee4f26ac3e3315095d80738c8426b1e2f4
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-27 13:16:21 -0500

    added strawberry block

--- snake.py
+++ snake.py
@@ -1,11 +1,13 @@
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
 
 screen = pygame.display.set_mode((1000, 800))
 block = pygame.image.load("./block.jpeg").convert()
+food = pygame.image.load("./strawberry_30_by_30.jpeg").convert()
 
 # 1. Define snake segments (list of lists)
 # Each [x, y] is a block. The first one is the head.
@@ -15,6 +17,9 @@ step = 30 # Size of your block
 
 clock = pygame.time.Clock() # To control game speed
 player_speed = 5
+
+food_x = 300
+food_y = 300
 
 running = True
 while running:
@@ -38,10 +43,19 @@ while running:
     # Add new head position to the front
     snake_body.insert(0, [head_x, head_y])
     # Remove the last segment (tail) so it doesn't grow infinitely
-    snake_body.pop()
+    if head_x == food_x and head_y == food_y:
+        # Don't pop the tail, so the snake grows!
+        # Move food to a new random location
+        food_x = random.randint(0, (1000 // step) - 1) * step
+        food_y = random.randint(0, (800 // step) - 1) * step
+    else:
+        # Remove the last segment if no food was eaten
+        snake_body.pop()
 
-    # 4. Rendering
-    screen.fill((0,100,0))
+    screen.fill((0, 100, 0))
+    
+    # Draw the strawberry
+    screen.blit(food, (food_x, food_y))
     for segment in snake_body:
         screen.blit(block, (segment[0], segment[1]))
     
